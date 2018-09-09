1df3a1d14ad567585aa4f15467acca7835190c68
ziven <ziven@zerone.io>
2018-09-09 23:18:07 +0800

    Upgraded cJSON to 1.7.7, updated module and related makefile

--- libs/build/cjson/README.md
+++ libs/build/cjson/README.md
@@ -1,3 +1,11 @@
-# cJSON-1.0
+# cJSON
 
-[source](http://sourceforge.net/projects/cjson)
+[Homepage](https://github.com/DaveGamble/cJSON)
+
+[Old v1.0](http://sourceforge.net/projects/cjson)
+
+Latest stable release: [1.7.7](https://github.com/DaveGamble/cJSON/archive/v1.7.7.tar.gz)
+
+## References
+
+- [28 个 C/C++ JSON评测](https://www.zhihu.com/question/23654513)
--- modules/cjson.mk
+++ modules/cjson.mk
@@ -3,19 +3,24 @@
 #
 # Module CJSON: cJSON is implemented in pure C language and lightweight
 #
-# Copyright (c) 2013-2015 ZeroneIO. All rights reserved.
+# Copyright (c) 2013-2018 ZeroneIO. All rights reserved.
 #
 # $Log: $
 #
 #
 
 CJSON_SDK   = $(ROOT)/libs/build/cjson
-CJSON_ROOT  = $(ROOT)/libs/cjson-1.0
-CJSON_INCDIRS := $(CJSON_ROOT)/include
-CJSON_LIBDIRS := $(CJSON_ROOT)/$(OSTYPE)/lib
-CJSON_LIBNAME := cjson
-CJSON_DEPENDS := $(CJSON_LIBDIRS)/libcjson.a
 CJSON_DEFINES := USE_CJSON
+CJSON_LIBNAME := cjson
+
+# Use libs built from source
+ifeq (, $(CJSON_ROOT))
+  CJSON_ROOT 	  := $(ROOT)/libs/cjson-1.7.7
+  CJSON_INCDIRS := $(CJSON_ROOT)/include
+  CJSON_LIBDIRS := $(CJSON_ROOT)/$(OSTYPE)/lib
+  CJSON_LDPATH  := $(CJSON_LIBDIRS)
+  CJSON_DEPENDS := $(CJSON_LIBDIRS)/libcjson.a
+endif
 
 $(CJSON_DEPENDS):
 	$(call make_module,CJSON,$(CJSON_SDK))
