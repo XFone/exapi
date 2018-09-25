20dba41d52d5a22523c3cc2333f5bf9d5a89242b
ziven <ziven@zerone.io>
2018-09-25 23:52:58 +0800

    Added type_name() to demangle C++ type's name for print out

--- src/iutils/osdeps/osutil.h
+++ src/iutils/osdeps/osutil.h
@@ -33,13 +33,57 @@
     }
 #endif /* _assert_PTHREAD */
 
-
+
+/*-------------------------- cplusplus -------------------------------*/
+
+#if defined(__cplusplus) && (__cplusplus >= 201103L)
+
+#include <string>
+#include <typeinfo>
+
+/**
+ * type_name(T) is to demangle c++ type name for print out
+ * This function is to replace the typeid(T).name(), only supported in C++11
+ * <pre>
+ *   try {
+ *      // some exceptions ocur...
+ *   } catch (exception &ex) {
+ *      cout << "exception caught " << type_name(ex).data() << ": " << ex.what()); 
+ *   }
+ * </pre>
+ */
+template<typename T> std::string type_name(T &t);
+
+#ifdef __GNUG__
+# include <cxxabi.h>
+# include <cstdlib>
+# include <memory>
+template<typename T>
+std::string type_name(T &t) {
+    int status;
+    std::unique_ptr<char[], decltype(&std::free)> 
+    buffer(__cxxabiv1::__cxa_demangle(typeid(t).name(), nullptr, 0, &status), &std::free);
+    return (status == 0) ? buffer.get() : 
+            /*"__cxa_demangle error"*/ typeid(t).name();
+}
+#else // !defined __GNUG__
+template<typename T>
+std::string type_name(T &t) {
+    return typeid(t).name();
+}
+#endif //__GNUG__
+
+#endif /* __cplusplus >= 201103L */
+
+/*-------------------------- C Functions -----------------------------*/
 
 BEGIN_EXTERN_C
 
+/**
+ * assert in thread-safe
+ */
 BOOL _assert_thread_op(int retval, unsigned *retry,
                        const char *func, const char *file, unsigned line);
-
 
 
 void _check_func(const char * file, int line, const char * msg);
